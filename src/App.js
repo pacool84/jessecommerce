34cf440bd51be71637b10debeb06edc52bdd3b48
Francisco López <flopez_1384@hotmail.com>
2020-08-21 10:40:08 -0500

    [ADD] React Router and first routes to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello my friend Jessecomerce</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Hello My friend checkout</h1>
+          </Route>
+          <Router path="/login">
+            <h1>Hello my friend Login</h1>
+          </Router>
+          <Route path="/">
+            <h1>Hello my friend HOMEPAGE</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
