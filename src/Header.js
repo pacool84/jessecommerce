7021cf7589a9dd06e0244686464e074cefdc249a
Francisco López <flopez_1384@hotmail.com>
2020-08-22 09:41:06 -0500

    [ADD] Some styles to the Header component

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Luimars from "./assets/Luimars-logo.png";
+import SearchIcon from "@material-ui/icons/Search";
 import "./styles/header.css";
 
 function Header() {
@@ -9,7 +10,16 @@ function Header() {
       <Link to="/">
         <img className="header__logo" src={Luimars} alt="logo page"></img>
       </Link>
-      <input type="text" className="header__searchbar"></input>
+      <div className="header__search">
+        <input type="text" className="header__searchbar"></input>
+        <SearchIcon className="header__searchicon"></SearchIcon>
+      </div>
+      <div className="header__nav">
+        <Link></Link>
+        <Link></Link>
+        <Link></Link>
+        <Link></Link>
+      </div>
     </nav>
   );
 }
