bcb4d802fdd4ec28d823006ac2962162c7326fd1
Francisco López <flopez_1384@hotmail.com>
2020-08-28 17:43:44 -0500

    [ADD] Link options to the Header component

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Luimars from "./assets/Luimars-logo.png";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasket from "@material-ui/icons/ShoppingBasket";
 import "./styles/header.css";
 
 function Header() {
@@ -15,10 +16,33 @@ function Header() {
         <SearchIcon className="header__searchicon"></SearchIcon>
       </div>
       <div className="header__nav">
-        <Link></Link>
-        <Link></Link>
-        <Link></Link>
-        <Link></Link>
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingBasket />
+            <span>0</span>
+          </div>
+        </Link>
       </div>
     </nav>
   );
